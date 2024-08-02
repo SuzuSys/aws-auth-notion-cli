3025a38d4b1a241abcf3d321a8b123c76cdb544b
SuzuSys <sigmatics@outlook.jp>
2024-08-02 17:49:59 +0900

    update setenv.ts

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,25 @@
 import { Client } from "@notionhq/client";
 import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import dotenv from "dotenv";
+import { setToken, setDatabaseID } from "./setenv";
 
-dotenv.config();
-
-const notion = new Client({
-  auth: process.env.NOTION_TOKEN,
-});
+dotenv.config({ override: true });
 
 (async () => {
+  if (!process.env.NOTION_TOKEN) {
+    const token = await setToken();
+    process.env.NOTION_TOKEN = token;
+  }
+  const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+  let database_id: string;
+  if (!process.env.DATABASE_IDS) {
+    database_id = await setDatabaseID();
+    process.env.DATABASE_IDS = database_id;
+  }
   const response = await notion.databases.query({
-    database_id: process.env.DATABASE_ID as string,
+    database_id: process.env.DATABASE_IDS as string,
   });
 
   let results: Array<PageObjectResponse>;
